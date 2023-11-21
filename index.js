6def54efb77c96a3cafe2765107be82faed52a0f
saeed <saeed00014@gmail.com>
2023-11-21 17:26:26 +0330

    first

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import path from "path";
 import mongoose from "mongoose";
-import * as dotenv from 'dotenv';
-dotenv.config();
+// import * as dotenv from 'dotenv';
+// dotenv.config();
 
 import authRoutes from "./api/routes/auths.js";
 import userRoutes from "./api/routes/users.js";
@@ -16,67 +16,98 @@ import likeRoutes from "./api/routes/likes.js";
 import messagesRoute from "./api/routes/messages.js";
 import friendRuotes from "./api/routes/friends.js";
 import productRuotes from "./api/routes/product.js";
-
 const app = express();
 
-//middlewares
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Credentials", true);
-  next();
-});
-app.use(express.json());
-app.use(bodyParser.json());
-app.use(
-  cors({
-    origin: ["*"]
-  })
-);
-app.use(cookieParser());
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './src/assets/avatar')
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname)
-  }
-})
-
-const upload = multer({ storage: storage })
-
-app.post('/i', upload.single('image'), (req, res) => {
-  res.send(req.file.filename)
-});
+app.use(cors())
 
 const __dirname = path.resolve();
-
 app.use("/", express.static(path.join(__dirname, "./dist")));
-app.use("/", express.static(path.join(__dirname, "./src/pages/sma/dist")));
-
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/posts", postRoutes);
-app.use("/api/comments", commentRoutes);
-app.use("/api/likes", likeRoutes);
-app.use("/api/messages", messagesRoute);
-app.use("/api/friends", friendRuotes);
-
-mongoose.connect(
-  process.env.MONGO_URL,
-  {
-      writeConcern: { w: 'majority' },
-  }
-);
-
-app.use('/products', productRuotes);
-  
-app.get("/sma/*", (_req, res) => {
-  res.sendFile(path.join(__dirname, "./src/pages/sma/dist", "index.html"));
-})
+app.get("/api/v1", (req, res) => {
+  res.json({
+    project: "React and Express Boilerplate",
+    from: "Vanaldito",
+  });
+});
 
 app.get("/*", (_req, res) => {
   res.sendFile(path.join(__dirname, "./dist", "index.html"));
 })
+const PORT = 4000
+app.listen(PORT, () => {
+  console.log();
+  console.log(`  App running in port ${PORT}`);
+  console.log();
+  console.log(`  > Local: \x1b[36mhttp://localhost:\x1b[1m${PORT}/\x1b[0m`);
+});
 
-app.listen(5500);
+// const app = express();
+
+// //middlewares
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Credentials", true);
+//   next();
+// });
+// app.use(express.json());
+// app.use(bodyParser.json());
+// app.use(
+//   cors({
+//     origin: ["http://localhost:5173",
+//     "http://localhost:5174"
+//   ]
+//   })
+// );
+// app.use(cookieParser());
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, './src/assets/avatar')
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, Date.now() + '-' + file.originalname)
+//   }
+// })
+
+// const upload = multer({ storage: storage })
+
+// app.post('/i', upload.single('image'), (req, res) => {
+//   res.send(req.file.filename)
+// });
+
+// const __dirname = path.resolve();
+
+// app.use("/", express.static(path.join(__dirname, "./dist")));
+// app.use("/", express.static(path.join(__dirname, "./src/pages/sma/dist")));
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/users", userRoutes);
+// app.use("/api/posts", postRoutes);
+// app.use("/api/comments", commentRoutes);
+// app.use("/api/likes", likeRoutes);
+// app.use("/api/messages", messagesRoute);
+// app.use("/api/friends", friendRuotes);
+
+// mongoose.connect(
+//   process.env.MONGO_URL,
+//   {
+//       writeConcern: { w: 'majority' },
+//   }
+// );
+
+// app.use('/products', productRuotes);
+  
+// app.get("/sma/*", (_req, res) => {
+//   res.sendFile(path.join(__dirname, "./src/pages/sma/dist", "index.html"));
+// })
+
+// app.get("/*", (_req, res) => {
+//   res.sendFile(path.join(__dirname, "./dist", "index.html"));
+// })
+
+
+// const hostname = '127.0.0.1';
+// const port = 8000;
+
+// app.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`)
+// });
 
