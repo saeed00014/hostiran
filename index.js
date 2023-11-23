894c91a980856f5995c6b4d97816c59ce003a219
saeed <saeed00014@gmail.com>
2023-11-23 09:07:37 +0330

    fff

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require("path");
 // const mongoose = require("mongoose");
 //require("dotenv").config();
 
-// const authRoutes = require("./api/routes/auths.js");
+const authRoutes = require("./api/routes/auths.js");
 // const userRoutes = require("./api/routes/users.js");
 // const postRoutes = require("./api/routes/posts.js");
 // const commentRoutes = require("./api/routes/comments.js");
@@ -45,7 +45,7 @@ app.use(cors());
 
 //app.use("/sma", express.static(path.join(__dirname, "./src/pages/sma/dist")));
 
-// app.use("/api/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 // app.use("/api/users", userRoutes);
 // app.use("/api/posts", postRoutes);
 // app.use("/api/comments", commentRoutes);
