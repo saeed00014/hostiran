7db696e8afc0059a71c113f5d4fe00de0e6fb7d1
saeed <saeed00014@gmail.com>
2023-11-23 09:19:35 +0330

    fff

--- index.js
+++ index.js
@@ -7,14 +7,19 @@ const path = require("path");
 // const mongoose = require("mongoose");
 //require("dotenv").config();
 
-//const authRoutes = require("./api/routes/auths.js");
-// const userRoutes = require("./api/routes/users.js");
-// const postRoutes = require("./api/routes/posts.js");
-// const commentRoutes = require("./api/routes/comments.js");
-// const likeRoutes = require("./api/routes/likes.js");
-// const messagesRoute = require("./api/routes/messages.js");
-// const friendRuotes = require("./api/routes/friends.js");
-//const productRuotes = require("./api/routes/product.js");
+app.use("/", express.static(path.join(__dirname, "./dist")));
+app.get("/*", (_req, res) => {
+  res.sendFile(path.join(__dirname, "./dist", "index.html"));
+})
+
+const authRoutes = require("./api/routes/auths.js");
+const userRoutes = require("./api/routes/users.js");
+const postRoutes = require("./api/routes/posts.js");
+const commentRoutes = require("./api/routes/comments.js");
+const likeRoutes = require("./api/routes/likes.js");
+const messagesRoute = require("./api/routes/messages.js");
+const friendRuotes = require("./api/routes/friends.js");
+const productRuotes = require("./api/routes/product.js");
 
 const app = express();
 
@@ -43,20 +48,15 @@ app.use(cors());
 //   res.send(req.file.filename)
 // });
 
-//app.use("/sma", express.static(path.join(__dirname, "./src/pages/sma/dist")));
 
-//app.use("/api/auth", authRoutes);
-// app.use("/api/users", userRoutes);
-// app.use("/api/posts", postRoutes);
-// app.use("/api/comments", commentRoutes);
-// app.use("/api/likes", likeRoutes);
-// app.use("/api/messages", messagesRoute);
-// app.use("/api/friends", friendRuotes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/likes", likeRoutes);
+app.use("/api/messages", messagesRoute);
+app.use("/api/friends", friendRuotes);
 
-app.use("/", express.static(path.join(__dirname, "./dist")));
-app.get("/*", (_req, res) => {
-  res.sendFile(path.join(__dirname, "./dist", "index.html"));
-})
 // mongoose.connect(
   //   process.env.MONGO_URL,
   //   {
