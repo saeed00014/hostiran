aefa939579efc20caafd7e6eee5bb69dfb40d220
saeed <saeed00014@gmail.com>
2023-11-23 09:00:43 +0330

    fff

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
 const express = require("express");
 const cors = require("cors");
-const multer = require("multer");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
+// const multer = require("multer");
+// const cookieParser = require("cookie-parser");
+//const bodyParser = require("body-parser");
 const path = require("path");
-const mongoose = require("mongoose");
-require("dotenv").config();
+// const mongoose = require("mongoose");
+//require("dotenv").config();
 
-const authRoutes = require("./api/routes/auths.js");
-const userRoutes = require("./api/routes/users.js");
-const postRoutes = require("./api/routes/posts.js");
-const commentRoutes = require("./api/routes/comments.js");
-const likeRoutes = require("./api/routes/likes.js");
-const messagesRoute = require("./api/routes/messages.js");
-const friendRuotes = require("./api/routes/friends.js");
-const productRuotes = require("./api/routes/product.js");
+// const authRoutes = require("./api/routes/auths.js");
+// const userRoutes = require("./api/routes/users.js");
+// const postRoutes = require("./api/routes/posts.js");
+// const commentRoutes = require("./api/routes/comments.js");
+// const likeRoutes = require("./api/routes/likes.js");
+// const messagesRoute = require("./api/routes/messages.js");
+// const friendRuotes = require("./api/routes/friends.js");
+// const productRuotes = require("./api/routes/product.js");
 
 const app = express();
 
 //middlewares
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Credentials", true);
-  next();
-});
-app.use(express.json());
-app.use(bodyParser.json());
-app.use(
-  cors()
-);
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Credentials", true);
+//   next();
+// });
+//app.use(express.json());
+//app.use(bodyParser.json());
+app.use(cors());
 // app.use(cookieParser());
 
 // const storage = multer.diskStorage({
@@ -48,13 +46,13 @@ app.use(
 //app.use("/sma", express.static(path.join(__dirname, "./src/pages/sma/dist")));
 app.use("/", express.static(path.join(__dirname, "./dist")));
 
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/posts", postRoutes);
-app.use("/api/comments", commentRoutes);
-app.use("/api/likes", likeRoutes);
-app.use("/api/messages", messagesRoute);
-app.use("/api/friends", friendRuotes);
+// app.use("/api/auth", authRoutes);
+// app.use("/api/users", userRoutes);
+// app.use("/api/posts", postRoutes);
+// app.use("/api/comments", commentRoutes);
+// app.use("/api/likes", likeRoutes);
+// app.use("/api/messages", messagesRoute);
+// app.use("/api/friends", friendRuotes);
 
 // mongoose.connect(
   //   process.env.MONGO_URL,
@@ -70,7 +68,7 @@ app.use("/api/friends", friendRuotes);
   })
 
 const hostname = '127.0.0.1';
-const port = 8000;
+const port = 4000;
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`)
