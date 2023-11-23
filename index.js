6edf9ee89d6d0e312b6cf114d32cf3adb4c4a496
saeed <saeed00014@gmail.com>
2023-11-23 09:45:09 +0330

    fff

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 
-// const authRoutes = require("./api/routes/auths.js");
-// const userRoutes = require("./api/routes/users.js");
-// const postRoutes = require("./api/routes/posts.js");
-// const commentRoutes = require("./api/routes/comments.js");
-// const likeRoutes = require("./api/routes/likes.js");
-// const messagesRoute = require("./api/routes/messages.js");
-// const friendRuotes = require("./api/routes/friends.js");
+const authRoutes = require("./api/routes/auths.js");
+const userRoutes = require("./api/routes/users.js");
+const postRoutes = require("./api/routes/posts.js");
+const commentRoutes = require("./api/routes/comments.js");
+const likeRoutes = require("./api/routes/likes.js");
+const messagesRoute = require("./api/routes/messages.js");
+const friendRuotes = require("./api/routes/friends.js");
 // const productRuotes = require("./api/routes/product.js");
 
 const app = express();
@@ -31,35 +31,31 @@ app.use(
 );
 app.use(cookieParser());
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './src/assets/avatar')
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname)
-  }
-})
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, './src/assets/avatar')
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, Date.now() + '-' + file.originalname)
+//   }
+// })
 
-const upload = multer({ storage: storage })
+// const upload = multer({ storage: storage })
 
-app.post('/i', upload.single('image'), (req, res) => {
-  res.send(req.file.filename)
-});
+// app.post('/i', upload.single('image'), (req, res) => {
+//   res.send(req.file.filename)
+// });
 
 //app.use("/sma", express.static(path.join(__dirname, "./src/pages/sma/dist")));
-app.use("/", express.static(path.join(__dirname, "./dist")));
-  
-//app.use("/api/auth", console.log('req'));
-// app.use("/api/users", userRoutes);
-// app.use("/api/posts", postRoutes);
-// app.use("/api/comments", commentRoutes);
-// app.use("/api/likes", likeRoutes);
-// app.use("/api/messages", messagesRoute);
-// app.use("/api/friends", friendRuotes);
 
-app.get("/*", (_req, res) => {
-  res.sendFile(path.join(__dirname, "./dist", "index.html"));
-})
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/likes", likeRoutes);
+app.use("/api/messages", messagesRoute);
+app.use("/api/friends", friendRuotes);
+
 // mongoose.connect(
 //   process.env.MONGO_URL,
 //   {
@@ -68,6 +64,11 @@ app.get("/*", (_req, res) => {
   //   );
   
 //app.use('/products', productRuotes);
+// app.use("/", express.static(path.join(__dirname, "./dist")));
+  
+// app.get("/*", (_req, res) => {
+//   res.sendFile(path.join(__dirname, "./dist", "index.html"));
+// })
 
 const hostname = '127.0.0.1';
 const port = 8000;
