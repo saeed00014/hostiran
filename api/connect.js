7cb485e388fe8d5fcafae902f691a8c0866a58d3
saeed <saeed00014@gmail.com>
2023-11-24 08:07:15 +0330

    fff

--- api/connect.js
+++ api/connect.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host:"https://saeedwebdev.ir/",
+  host:"saeedwebdev.ir/",
   user:"saeedwe1_root",
   password:"amiralim890",
   database:"saeedwe1_social"
