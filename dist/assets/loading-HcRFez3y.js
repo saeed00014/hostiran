dd8d780e75c4d686d6e227225f5bb1725943464d
saeed <saeed00014@gmail.com>
2023-11-29 13:07:21 +0330

    fff

---
+++ dist/assets/loading-HcRFez3y.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-3ow1JuyA.js";const e=()=>n.jsx("div",{className:"SideLoadingContainer",children:n.jsx("p",{children:n.jsx("span",{})})});export{e as S};
--- src/pages/sma/components/AcountInfo.jsx
+++ src/pages/sma/components/AcountInfo.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 import defaultAvatar from '../assets/images/testImg.png'
 
@@ -7,7 +7,6 @@ import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 const AcountInfo = () => {
-  const [avatar, setAvatar] = useState(null)
   const ui = useSelector((state) => state.ui)
   const loginUser = ui.loginUser
 
@@ -16,7 +15,7 @@ const AcountInfo = () => {
       <div className='flex items-center justify-center w-full p-2 rounded-full gap-3 xl:hover:bg-bg-hover'>
         <Link to={`/sma/profile/${loginUser[0] && loginUser[0].id}`} className='flex items-center xl:justify-start justify-center gap-3 w-full'>
           <div className='xl:w-[50px] xl:h-[50px] h-10 w-10 xl:mr-0 md:-mr-4 bg-white rounded-full'>
-            <img src={targetUser[0].avatar ? `https://saeedwebdev.ir/avatar/${targetUser[0].avatar}` : defaultAvatar} alt="" className='object-cover md:min-w-[50px] md:min-h-[50px] min-w-[40px] min-h-[40px] rounded-full'/>
+            <img src={loginUser[0].avatar ? `https://saeedwebdev.ir/avatar/${loginUser[0].avatar}` : defaultAvatar} alt="" className='object-cover md:min-w-[50px] md:min-h-[50px] min-w-[40px] min-h-[40px] rounded-full'/>
           </div>
           <div className='xl:flex hidden flex-col w-full min-w-max'>
             <span>{loginUser[0] && (loginUser[0].firstName + ' ' + loginUser[0].lastName)}</span>
